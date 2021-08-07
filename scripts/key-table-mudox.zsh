1c6df379094268ccd1f6c0d8f2f6da4480b695ca
mudox <imudox@gmail.com>
2021-08-08 04:46:07 +0800

    key-table-mudox: auto select the 1st pane in `switch-client`

--- scripts/key-table-mudox.zsh
+++ scripts/key-table-mudox.zsh
@@ -5,10 +5,10 @@ set -euo pipefail
 tmux bind-key - switch-client -T mudox
 
 typeset -A sessions=(
-  d Dotfiles
-  h Hammerspoon
-  n Neovim
-  t Tmux
+  d Dotfiles:1
+  h Hammerspoon:1
+  n Neovim:1
+  t Tmux:1
 )
 
 for key name in "${(@kv)sessions}"; do
