436e22116a75fee0d76e1768e78ab3ec32753979
mudox <imudox@gmail.com>
2021-09-14 16:34:06 +0800

    add window split to session `Tmux`

--- sessions/Tmux.tmux-session.zsh
+++ sessions/Tmux.tmux-session.zsh
@@ -9,4 +9,10 @@ root_dir="${MDX_TMUX_DIR}"
 new_session Main "${root_dir}" nvim
 title_pane 1 Neovim
 
+tmux split-window \
+  -h \
+  -t "${window}.1" \
+  -c "${root_dir}/sessions"
+title_pane 2 Sessions
+
 clean_up
