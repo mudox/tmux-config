0a0f910ad14e19aa9a29ee391e1d763922f9a4fe
mudox <imudox@gmail.com>
2021-10-23 07:08:19 +0800

    add `Help` to session `Tmux`

--- sessions/Tmux.tmux-session.zsh
+++ sessions/Tmux.tmux-session.zsh
@@ -9,10 +9,6 @@ root_dir="${MDX_TMUX_DIR}"
 new_session Main "${root_dir}" nvim
 title_pane 1 Neovim
 
-tmux split-window \
-  -h \
-  -t "${window}.1" \
-  -c "${root_dir}/sessions"
-title_pane 2 Sessions
+new_window Help "${root_dir}" 'MDX_NVIM_MODE=help man tmux'
 
 clean_up
