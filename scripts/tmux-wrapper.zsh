68a7061f568796ec2d191c8cfb7c601b1066469d
mudox <imudox@gmail.com>
2021-12-20 20:35:29 +0800

    tmux-wrapper: add check for `vscode` environment

--- scripts/tmux-wrapper.zsh
+++ scripts/tmux-wrapper.zsh
@@ -8,27 +8,29 @@ cmd=("/opt/homebrew/bin/tmux") # HEAD
 # Determine server
 server=default
 if [[ -n $ITERM_SESSION_ID ]]; then
-    server=iterm
+  server=iterm
 elif [[ -n $KITTY_WINDOW_ID ]]; then
-    server=kitty
+  server=kitty
 elif [[ -n $ALACRITTY_LOG ]]; then
-    server=alacritty
+  server=alacritty
 elif [[ -n $HYPER ]]; then
-    server=hyper
+  server=hyper
+elif [[ $TERM_PROGRAM = vscode ]]; then
+  server=vscode
 fi
 cmd+=(-L $server)
 
 if (($# > 0)); then
-    if [[ $1 = which-server ]]; then
-        echo "$server"
-    else
-        exec $cmd $@
-    fi
+  if [[ $1 = which-server ]]; then
+    echo "$server"
+  else
+    exec $cmd $@
+  fi
 else
-    # Attach or launch
-    $cmd attach >/dev/null 2>&1
-    if (($? != 0)); then
-        echo 'creating intial session ...'
-        exec $cmd new-session -s Default -n Main
-    fi
+  # Attach or launch
+  $cmd attach >/dev/null 2>&1
+  if (($? != 0)); then
+    echo 'creating intial session ...'
+    exec $cmd new-session -s Default -n Main
+  fi
 fi
