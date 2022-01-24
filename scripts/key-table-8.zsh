09cece23e5844b4f708e786a7374ff5c2f509c8f
mudox <imudox@gmail.com>
2022-01-24 13:56:13 +0800

    change key table `8` to `,`

--- scripts/key-table-8.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-set -euo pipefail
-
-mid='eight'
-tmux bind-key 8 switch-client -T "$mid"
-
-# respawn current pane with `zsh`
-tmux bind-key -T"$mid" z respawn-pane -k zsh
-
---
+++ scripts/key-table-comma.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+set -euo pipefail
+
+mid='comma'
+tmux bind-key , switch-client -T "$mid"
+
+bind() {
+  tmux bind-key -T "$mid" "$@"
+}
+
+# respawn current pane with `zsh`
+bind z respawn-pane -k 'zsh'
+
+# respawn current pane with `zsh`
+bind r respawn-pane -k 'ap -t'
