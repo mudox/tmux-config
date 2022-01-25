70d050199248163c2b70f6b37493d56e84a1ea89
mudox <imudox@gmail.com>
2022-01-26 04:09:36 +0800

    feat: update titles of panes from current window

--- scripts/hooks/after-split-window.zsh
+++ scripts/hooks/after-split-window.zsh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 set -euo pipefail
 
+source "${MDX_TMUX_DIR}/scripts/lib/utils.zsh"
+
+# give the new pane a default title
+"${scripts_dir}"/title-panes.zsh
+
 # reveal pane border after splitting window
 tmux set-option -w pane-border-status top
---
+++ scripts/title-panes.zsh
@@ -0,0 +1,30 @@
+#!/usr/bin/env zsh
+set -euo pipefail
+
+source "${MDX_TMUX_DIR}/scripts/lib/utils.zsh"
+
+set-title() {
+  title=$(tmux display-message -t "$1" -p '#T')
+  default_title=$(tmux display-message -p '#{host}')
+
+  if [[ $title != $default_title ]]; then
+    return
+  fi
+
+  cmd=$(tmux display-message -t "$1" -p '#{pane_current_command}')
+
+  case $cmd in
+  *zsh*)
+    title='  Shell'
+    ;;
+  *nvim*)
+    title='  Edit'
+    ;;
+  esac
+
+  tmux select-pane -t "$1" -T "${title}"
+}
+
+for id in $(tmux list-panes -F '#D'); do
+  set-title "${id}"
+done
