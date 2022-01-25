81b89634e99c0b389d194078a6b16ef3ae4f2fe6
mudox <imudox@gmail.com>
2022-01-25 22:16:56 +0800

    feat: auto load session theme

---
+++ scripts/load-session-theme.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+set -euo pipefail
+
+source "${MDX_TMUX_DIR}/scripts/lib/utils.zsh"
+
+set -x
+theme_path="${session_themes_dir}/$(get_session_name)"
+
+if [[ -f $theme_path ]]; then
+  tmux source-file "${theme_path}"
+else
+  tmux source-file "${session_themes_dir}/Default"
+fi
