29dc3b5be21b7e97d289c48e81411d171982c51e
mudox <imudox@gmail.com>
2022-03-06 02:25:29 +0800

    do not change kitty font size

--- scripts/hooks/after-resize-pane.zsh
+++ scripts/hooks/after-resize-pane.zsh
@@ -8,9 +8,7 @@ zoomed=$(tmux display-message -p '#{window_zoomed_flag}')
 if (( zoomed )); then
 	tmux set-option -w status off
   tmux set-option -w pane-border-status off
-	kitty @ --to "${KITTY_LISTEN_ON}" set-font-size 22
 else
-	# kitty @ --to "${KITTY_LISTEN_ON}" set-font-size 0
 	tmux set-option -w status on
 	"${scripts_dir}/update-pane-border-status.zsh"
 fi
