b82c00a7ff8567dc22387de2ce94f81263d969f6
mudox <imudox@gmail.com>
2022-03-09 03:51:48 +0800

    change `+V` to launch nvim in `coc` mode instead of `lsp` mode

--- scripts/respawn-pane-with-neovim-in-coc-mode.zsh
+++ scripts/respawn-pane-with-neovim-in-coc-mode.zsh
@@ -4,5 +4,5 @@ set -euo pipefail
 source "${MDX_TMUX_DIR}/scripts/lib/utils.zsh"
 
 tmux clear-history
-set_pane_label_suffix current '  Edit'
-tmux respawn-pane -k 'MDX_NVIM_MODE=lsp nvim'
+set_pane_label_suffix current '  Mode: CoC'
+tmux respawn-pane -k 'MDX_NVIM_MODE=coc nvim'
