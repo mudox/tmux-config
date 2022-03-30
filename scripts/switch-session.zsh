8084783bebe5694f8c20b88842884aee23c5d8c9
mudox <imudox@gmail.com>
2022-03-30 13:09:24 +0800

    fix typo

--- scripts/switch-session.zsh
+++ scripts/switch-session.zsh
@@ -2,10 +2,9 @@
 set -euo pipefail
 
 session=${1%:*}
-window=${1#*:}
 
 if ! tmux has-session -t "${session}"; then
-  bin="${MDX_TMUX_DIR}/sessions/${sessions}.tmux-session.zsh"
+  bin="${MDX_TMUX_DIR}/sessions/${session}.tmux-session.zsh"
   if [[ -f ${bin} ]]; then
     "${bin}" >/dev/null 2>&1 && tmux switch-client -t "$1"
   fi
