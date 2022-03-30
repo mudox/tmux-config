553acc7929a593a191697d99d9f71e51dd178f8e
mudox <imudox@gmail.com>
2022-03-30 10:41:15 +0800

    add `Journal` window to `Neorg` session

--- sessions/Neorg.tmux-session.zsh
+++ sessions/Neorg.tmux-session.zsh
@@ -11,7 +11,17 @@ session "Neorg"
 local window_name='Main'
 local pane_title='  Edit'
 local dir="$root_dir"
-local cmd='MDX_NVIM_MODE=neorg nvim'
+local cmd='MDX_NVIM_MODE=neorg nvim inbox.norg'
+window
+}
+# 〉
+
+# Window: "Journal" 〈
+() {
+local window_name='Journal'
+local pane_title='  Journal'
+local dir="${root_dir}/journal"
+local cmd='MDX_NVIM_MODE=neorg nvim -O $(\ls | sort -r | head -n5)'
 window
 }
 # 〉
